f6f6d5630c70e062f80b77fb285baa48c09b2009
sanchit2843 <sanchittanwar75@gmail.com>
2019-06-07 14:01:12 +0530

    Data plot

---
+++ Dataplot.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import random
+#plotting rondom images from dataset
+def class_plot(n_figures , data , encoder ,inv_normalize = None):
+    n_row = int(n_figures/3)
+    fig,axes = plt.subplots(figsize=(14, 10), nrows = n_row, ncols=3)
+    for ax in axes.flatten():
+        a = random.randint(0,len(data))
+        (image,label) = data[a]
+        label = int(label)
+        l = encoder[label]
+        if(inv_normalize!=None):
+            image = inv_normalize(image)
+
+        image = image.numpy().transpose(1,2,0)
+        im = ax.imshow(image)
+        ax.set_title(l)
+        ax.axis('off')
+    plt.show()
+
+# To plot the wrong data predicted from the test set
+#True is label collected from test function of the actual label of wrong prediction
+#ima it is list of wrong predicted images
+#pred Predicted class of the images
+def wrong_plot(n_figures,true,ima,pred,encoder,inv_normalize):
+    print('Classes in order Actual and Predicted')
+    n_row = int(n_figures/3)
+    fig,axes = plt.subplots(figsize=(14, 10), nrows = n_row, ncols=3)
+    for ax in axes.flatten():
+        a = random.randint(0,len(true)-1)
+
+        image,correct,wrong = ima[a],true[a],pred[a]
+        image = torch.from_numpy(image)
+        correct = int(correct)
+        c = encoder[correct]
+        wrong = int(wrong)
+        w = encoder[wrong]
+        f = 'A:'+c + ',' +'P:'+w
+        if inv_normalize !=None:
+            image = inv_normalize(image)
+        image = image.numpy().transpose(1,2,0)
+        im = ax.imshow(image)
+        ax.set_title(f)
+        ax.axis('off')
+    plt.show()
