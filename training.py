dda277f2dc9652a058768b4579b087836c540900
sanchit2843 <sanchittanwar75@gmail.com>
2019-06-07 19:11:48 +0530

    util error

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
 import torch.optim as optim
-from image_classification.util import *
+from util import *
 from torch.autograd import Variable
 def train(model,dataloaders,criterion,num_epochs=10,lr=0.00001,batch_size=8,patience = None):
     since = time.time()
