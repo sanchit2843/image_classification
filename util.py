aa37dd673aef67edaab4dc3a9aa6b336465777ab
sanchit2843 <sanchittanwar75@gmail.com>
2019-07-15 09:36:21 +0530

    Update util.py

--- util.py
+++ util.py
@@ -149,3 +149,41 @@ def acc_plot(acc):
     plt.xlabel("epochs")
     plt.ylabel("accuracy")
     plt.show()
+
+def im_convert(tensor,inv_normalize):
+    """ Display a tensor as an image. """
+    image = tensor.to("cpu").clone().detach()
+    image = image.squeeze()
+    image = inv_normalize(image)
+    image= image.numpy()
+    image = image.transpose(1,2,0)
+    image = image.clip(0, 1)
+    return image
+def preprocess(path,test_transforms):
+  img = cv2.imread(path)
+  img = test_transforms(img)
+  img = img.unsqueeze(0)
+  return img
+def cmap(model,path):
+  img = preprocess(path)
+  fmap,logits = model(img.to('cuda'))
+  params = list(model.parameters())
+  weight_softmax = model.linear.weight.detach().cpu().numpy()
+  logits = sm(logits)
+  idx = np.argmax(logits.detach().cpu().numpy())
+  bz, nc, h, w = fmap.shape
+  out = np.dot(fmap.detach().cpu().numpy().reshape((nc, h*w)).T,weight_softmax[idx,:].T)
+  cam = out.reshape(h,w)
+  cam = cam - np.min(cam)
+  cam_img = cam / np.max(cam)
+  cam_img = np.uint8(255*cam_img)
+  out = cv2.resize(cam_img, (im_size,im_size))
+  heatmap = cv2.applyColorMap(out, cv2.COLORMAP_JET)
+  img = im_convert(img)
+  result = heatmap * 0.3 + img * 0.5
+  cv2.imwrite('/content/1.jpg',result)
+  print(encoder[idx])
+  plt.imshow(img,alpha = 0.8)
+  plt.imshow(out,cmap = 'jet',alpha = 0.5)
+  plt.show()
+  plt.savefig("/content/cmap.png")
